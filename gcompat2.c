7b897d8068fd96c0d506f45e958ecb1e23030398
phuslu <phus.lu@gmail.com>
2023-03-22 08:58:09 +0000

    add arconf

--- gcompat2.c
+++ gcompat2.c
@@ -1,5 +1,8 @@
 #include <wchar.h>
 #include <string.h>
+#include <stdlib.h>
+#include <pthread.h>
+
 /* The mbrlen function has an internal shift state which gets used if
    the PS parameter is NULL.  */
 static mbstate_t internal;
@@ -13,5 +16,16 @@ char *
 __strdup (const char *s)
 {
   return strdup(s);
+}
+
+int
+pthread_mutexattr_setkind_np (pthread_mutexattr_t * attr, int kind)
+{
+  return pthread_mutexattr_settype (attr, kind);
+}
+
+double __strtod_internal(const char * nptr, char * * endptr, int __group)
+{
+	return strtod(nptr, endptr);
 }
 
